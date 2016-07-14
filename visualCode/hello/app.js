bba7dae8254c6dd9b0d8116ea8d9649a44681556
Akshaya Kumar <akshayakumar@Akshayas-MacBook-Pro.local>
2016-07-14 14:33:27 +0530

    commit

--- visualCode/hello/app.js
+++ visualCode/hello/app.js
@@ -1,2 +1,17 @@
-var msg="hello world";
-console.log("<html><body>hi</body></html>");
+//3 initial steps
+//1. import modules
+//2. create server
+//3. Read request and return response
+
+//Import required modules
+var http=require("http");
+
+http.createServer(function(request,response){
+
+//200 - OK
+    response.writeHead(200,{'Context-Type': 'text/plain'});
+    response.end('hello world!\n');
+
+}).listen(8081);
+
+console.log("lalalla server working");
